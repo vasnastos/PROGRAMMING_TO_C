140888f6a3eefab602039dbb75250e0e83b330ae
nastos vasilis <63415169+vasnastos@users.noreply.github.com>
2020-06-21 14:03:07 +0300

    Add files via upload

--- ΕΠΑΝΑΛΗΠΤΙΚΕΣ_ΑΣΚΗΣΕΙΣ_5ο_ΣΕΤ/ΑΣΚΗΣΗ_3/ex3.c
+++ ΕΠΑΝΑΛΗΠΤΙΚΕΣ_ΑΣΚΗΣΕΙΣ_5ο_ΣΕΤ/ΑΣΚΗΣΗ_3/ex3.c
@@ -37,22 +37,105 @@ int getsize()
 
 void readata()
 {
-    //implemantation
+    char line[s];
+    int i=0;
+    FILE *fp=fopen(filename,"r");
+    while(fgets(line,s,fp)!=NULL)
+    {
+        if(line[0]=='#') continue;
+        if(line[0]=='\n') continue;
+
+        char *token=strtok(line,"-");
+        strcpy(accs[i].accnum,token);
+        token=strtok(NULL,"-");
+        strcpy(accs[i].ownername,token);
+        token=strtok(NULL,"-");
+        accs[i].balance=atof(token);
+        i++;
+    }
+    fclose(fp);
 }
 
 void deposit(char *accn)
 {
-    //implentation
+    int pos=-1;
+    for(int i=0;i<size;i++)
+    {
+        if(strcmp(accs[i].accnum,accn)==0)
+        {
+            pos=i;
+            break;
+        }
+    }
+    if(pos==-1)
+    {
+        perror("No such account found please enter a valid account number!!!\n");
+        return;    
+    }
+    double extra_balance;
+    printf("Give the extra amount you want to add to your account:");
+    scanf("%lf",&extra_balance);
+    accs[pos].balance+=extra_balance;
 }
 
-void withdraw(int pos)
+void withdraw(char *accn)
 {
-    //implementation
+    double available_amounts[]={20.0,40.0,60.0,100.0,200.0};
+    int pos=-1;
+    for(int i=0;i<size;i++)
+    {
+        if(strcmp(accs[i].accnum,accn)==0)
+        {
+            pos=i;
+            break;
+        }
+    }
+    if(pos==-1)
+    {
+        perror("No such account found please enter a valid account number!!!\n");
+        return;    
+    }
+    int op;
+    do{
+      printf("WITHDRAW MENU::::\n");
+      printf("WITHDRAW OPTIONS:::\n");
+      printf("1-20\t4-100.0\n");
+      printf("2-40.0\t5-200.0\n");
+      printf("3-60.0\t6-Cancel Transaction\n");
+      do
+      {
+         printf("Give option:");
+         scanf("%d",&op);
+         if(op==6)
+         {
+             return;
+         }
+      } while (op<1 || op>6);
+      if(accs[pos].balance<available_amounts[op-1])
+      {
+          printf("Invalid amount of withdraw,Please choose a smaller amount");
+      }
+    } while (accs[pos].balance<available_amounts[op-1]);
+    accs[pos].balance-=available_amounts[op-1];
 }
 
 void findbalance(char *accn)
 {
-    //implemantation
+    int pos=-1;
+    for(int i=0;i<size;i++)
+    {
+        if(strcmp(accs[i].accnum,accn)==0)
+        {
+            pos=i;
+            break;
+        }
+    }
+    if(pos==-1)
+    {
+        perror("No such account found please enter a valid account number!!!\n");
+        return;    
+    }
+    printf("BALANCE OF ACCOUNT NUMBER:%s is:%lf\n",accn,accs[pos].balance);
 }
 
 char* getinput()
@@ -65,7 +148,7 @@ char* getinput()
     {
         if(strcmp(accs[i].accnum,accn)==0)
         {
-            connected=(struct char *)malloc(s * sizeof(char))
+            connected=(char *)malloc(s * sizeof(char));
             strcpy(connected,accs[i].accnum);
             return connected;
         }
