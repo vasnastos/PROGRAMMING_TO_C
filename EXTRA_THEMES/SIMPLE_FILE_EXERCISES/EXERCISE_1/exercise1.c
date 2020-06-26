574e71839ed3749b532a5c654f36603fe7b5b22d
nastos vasilis <nastosvasileios99@gmail.com>
2020-06-26 11:25:39 +0300

    update

--- EXTRA_THEMES/SIMPLE_FILE_EXERCISES/EXERCISE_1/exercise1.c
+++ EXTRA_THEMES/SIMPLE_FILE_EXERCISES/EXERCISE_1/exercise1.c
@@ -1,2 +1,74 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
+
+char filename[100];
+
+void read_data(int argument)
+{
+    int cntmax=0;
+    int i=0;
+    double max;
+    double sum=0.0;
+    double number;
+    FILE *fp=fopen(filename,"r");
+    while(fscanf(fp,"%lf",&number)>0)
+    {
+        if(i==0)
+        {
+            max=number;
+            cntmax=1;
+        }
+        else if(number>max)
+        {
+            max=number;
+            cntmax=1;
+        }
+        else if(number==max)
+        {
+            cntmax++;
+        }
+        sum+=number;
+        i++;
+    }
+    fclose(fp);
+    double *board=(double *)malloc(i * sizeof(double));
+    i=0;
+    fp=fopen(filename,"r");
+    while(fscanf(fp,"%lf",&number)>0)
+    {
+        board[i]=number;
+        i++;
+    }
+    fclose(fp);
+    int cnt_arg=0;
+    printf("########RESULTS#########\n");
+    printf("max found on file:%d\n",cntmax);
+    for(int j=0;j<i;j++)
+    {
+        if(board[j]>argument)
+        {
+            cnt_arg++;
+        }
+        if(board[j]==max)
+        {
+           printf("Max found in position:%d\n",j+1);
+        }
+    }
+    printf("Numbers over %d argument are:%d",cnt_arg);
+    printf("SQRT of sum in file is:%.2lf\n",sqrt(sum));
+    printf("########################\n");
+    free(board);
+}
+
+int main(int argc,char *argv[])
+{
+    if(argc!=3)
+    {
+        perror("Please run again the program with the correct number of arguments!!!!\n");
+        return;
+    }
+    double argument=atof(argv[1]);
+    strcpy(filename,argv[2]);
+    read_data(argument);
+}
